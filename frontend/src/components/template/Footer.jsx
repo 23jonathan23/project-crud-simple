e54f0aeadea0e0302ea3271ab92c50120d9b17d9
Jonathan Raphael <jonathanraphaelz@MacBook-Pro-de-Jonathan.local>
2020-04-07 21:38:16 -0300

    Project-crud-simple-complete

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> Jonathan <span className="text-danger">Raphael</span></strong>
+    </span>
+  </footer>
